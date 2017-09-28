641f617f7f9c7634a8127253ffe0004c2aa13aae
MaimilianPichler <maximilian.pichler97@gmail.com>
2017-09-28 16:59:24 +0200

    .

--- src/SelectField.spec.js
+++ src/SelectField.spec.js
@@ -2,9 +2,5 @@ import expect from 'expect'
 
 
 describe('SelectField', () => {
-  it('should not crash', () => {
-    expect(
-      true)
-    ).toEqual(true)
-  })
+  it('should not crash', () => {expect(true).toEqual(true)})
 })
